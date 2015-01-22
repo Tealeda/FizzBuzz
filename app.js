12db56d86d8f748617a4bca9b4b03bceb842f4a0
Tealeda <tealedacoder@gmail.com>
2015-01-22 01:59:35 -0500

    Refactor FizzBuzz with prompt

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
 $(document).ready(function(){
-  
-	for (var i = 1; i <= 100; i++) {
+	
 
-	   if ((i % 3)== 0 && (i % 5) == 0 ) {
-	   		$("body").append("fizzbuzz");
-	   	} 
-	    
-	   else if ((i % 3)== 0 ) {
-	   		$("body").append("fizz");
-	   } 
-	    
-	   else if ((i % 5) == 0 ) {
-	   		$("body").append("buzz");
-	   } 
-	   
-	   else {
-	   		$("body").append(i);
-		} 
+	var max =  prompt("Please enter a number", +"");{
+		fizzbuzzer(max);
+    }
+	
+	function fizzbuzzer(max) {
+
+	    function check(n) {
+	      var msg = '';
+	      if ( n % 3 == 0 ) { msg += "Fizz" };
+	      if ( n % 5 == 0 ) { msg += "Buzz" };
+	      return msg || n;
+	    }
+
+	    for (var i = 1; i <= max; i++) {
+	      $("body").append(check(i) + '<br />');
+	    }
 	}
-  
 })
 
-// for (var i = 1; i <= 100; i++) {
+
+
+
+//Jfor loop examples:
+  
+// 	for (var i = 1; i <= 100; i++) {
 
 // 	   if ((i % 3)== 0 && (i % 5) == 0 ) {
-// 	   		$("body").append("fizzbuzz, ");
-// 	   	} 
-	    
-// 	   else if ((i % 3)== 0 ) {
-// 	   		$("body").append("fizz, ");
-// 	   } 
-	    
-// 	   else if ((i % 5) == 0 ) {
-// 	   		$("body").append("buzz, ");
-// 	   } 
-	   
-// 	   else {
-// 	   		$("body").append(i +", ");
+// 	   		$("body").append("fizzbuzz");
+// 	   	} else if ((i % 3)== 0 ) {
+// 	   		$("body").append("fizz");
+// 	   } else if ((i % 5) == 0 ) {
+// 	   		$("body").append("buzz");
+// 	   } else {
+// 	   		$("body").append(i + ', ');
 // 		} 
 // 	}
+  
+
+//     var i, text;
+    
+// 	for ( i = 0; i <= 100; i += 1) {
+//         text = '';
+        
+// 	   if ((i % 3) === 0 ) {
+// 	   		text += 'fizz';
+//        } 
+	    
+// 	   if ((i % 5) === 0 ) {
+//            text += 'buzz';
+//        } 
+//        $('body').append( i + ' ' + text + '<br />'); 
+// 	}
+  
